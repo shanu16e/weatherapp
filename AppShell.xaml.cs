0fc52b32883aca4b23ec770644dfd06a240e4eec
shanu16e <selvasuths@uni.coventry.ac.uk>
2025-06-28 17:21:36 +0100

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using $safeprojectname$.Views;
+
+
+namespace $safeprojectname$
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            // Register routes for navigation
+            Routing.RegisterRoute(nameof(CitySearchPage), typeof(CitySearchPage));
+        }
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+using $safeprojectname$.ViewModels;
+using $safeprojectname$.Views;
+using $safeprojectname$.Services;
+
+namespace $safeprojectname$
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            // Register services
+            builder.Services.AddSingleton<WeatherService>();
+            builder.Services.AddSingleton<IGeolocation>(Geolocation.Default);
+
+            // Register view models
+            builder.Services.AddSingleton<MainPageViewModel>();
+            builder.Services.AddTransient<CitySearchViewModel>();
+
+            // Register pages
+            builder.Services.AddSingleton<MainPage>();
+            builder.Services.AddTransient<CitySearchPage>();
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
